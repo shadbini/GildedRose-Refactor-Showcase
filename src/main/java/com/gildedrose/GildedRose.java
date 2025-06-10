13329b23aab1e461f72ca3b7cb3ffd7cf4913d0e
shadi <shadiabedini@gmail.com>
2025-06-10 17:18:56 +0200

    Refactor: start to restructure updateQuality(item) with positive condition branches per item type (name)

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -15,44 +15,44 @@ class GildedRose {
 
     private void updateItem(Item item) {
         updateQuality(item);
-
         updateSellIn(item);
-
-        if (item.sellIn < 0) {
+        if (isExpired(item)) {
             processItem(item);
         }
     }
 
-    private static void updateSellIn(Item item) {
+    private void updateSellIn(Item item) {
         if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
             item.sellIn = item.sellIn - 1;
         }
     }
 
     private void updateQuality(Item item) {
-        if (!item.name.equals("Aged Brie")
-                && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            if (item.quality > 0) {
-                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                    item.quality = item.quality - 1;
-                }
+        if (item.name.equals("Aged Brie")) {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
-        } else {
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality < 50) {
                 item.quality = item.quality + 1;
 
-                if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.sellIn < 11) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
-                    }
 
-                    if (item.sellIn < 6) {
-                        if (item.quality < 50) {
-                            item.quality = item.quality + 1;
-                        }
+                if (item.sellIn < 11) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
                     }
+                }
+
+                if (item.sellIn < 6) {
+                    if (item.quality < 50) {
+                        item.quality = item.quality + 1;
+                    }
+                }
+            }
+        } else {
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
                 }
             }
         }
@@ -74,5 +74,9 @@ class GildedRose {
                 item.quality = item.quality + 1;
             }
         }
+    }
+
+    private boolean isExpired(Item item) {
+        return item.sellIn < 0;
     }
 }
