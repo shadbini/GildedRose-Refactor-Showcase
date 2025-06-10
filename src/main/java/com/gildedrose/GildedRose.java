bbd81fbe6336e4201c5ea3aa615a8351ac3be65f
shadi <shadiabedini@gmail.com>
2025-06-10 18:36:29 +0200

    Refactor: extract increaseQuality() to reduce duplication, reorder conditional logic in processItem(item) for early return on Backstage

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -48,19 +48,15 @@ class GildedRose {
     }
 
     private void processItem(Item item) {
-        if (!item.name.equals("Aged Brie")) {
-            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                if (item.quality > 0) {
-                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                        item.quality = item.quality - 1;
-                    }
-                }
-            } else {
-                item.quality = 0;
-            }
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
         } else {
-            if (item.quality < 50) {
-                item.quality = item.quality + 1;
+            if (item.quality > 0) {
+                if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                    item.quality = item.quality - 1;
+                }
             }
         }
     }
@@ -71,7 +67,7 @@ class GildedRose {
 
     private void increaseQuality(Item item) {
         if (item.quality < 50) {
-            item.quality = item.quality + 1;
+            item.quality++;
         }
     }
 }
