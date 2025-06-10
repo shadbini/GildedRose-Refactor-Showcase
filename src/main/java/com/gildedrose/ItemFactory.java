acc5e145e76df0db3e7e0620dfeccc49cdd967d4
shadi <shadiabedini@gmail.com>
2025-06-10 19:02:37 +0200

    Extract item names as constants and integrate ItemFactory into GildedRose

--- src/main/java/com/gildedrose/ItemFactory.java
+++ src/main/java/com/gildedrose/ItemFactory.java
@@ -1,10 +1,16 @@
 package com.gildedrose;
 
 class ItemFactory {
+
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String BACKSTAGE_PASSES = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     public static ItemInventory create(Item item) {
-        if (item.name.equals("Aged Brie")) return new AgedBrieItem(item);
-        if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) return new BackstagePassItem(item);
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) return new SulfurasItem(item);
+        if (AGED_BRIE.equals(item.name)) return new AgedBrieItem(item);
+        if (BACKSTAGE_PASSES.equals(item.name)) return new BackstagePassItem(item);
+        if (SULFURAS.equals(item.name)) return new SulfurasItem(item);
         return new ItemInventory(item);
     }
 }
+
