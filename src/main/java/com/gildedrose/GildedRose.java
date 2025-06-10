77c38b41085a8ab26545507705e95346b3993a0d
shadi <shadiabedini@gmail.com>
2025-06-10 17:21:33 +0200

    Refactor: simplify updateSellIn() using early return for Sulfuras

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -22,9 +22,10 @@ class GildedRose {
     }
 
     private void updateSellIn(Item item) {
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
         }
+        item.sellIn--;
     }
 
     private void updateQuality(Item item) {
