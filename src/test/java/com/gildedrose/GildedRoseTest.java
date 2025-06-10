d73ae0abe6427154dc112e70a10a0ab7c0f0c569
shadi <shadiabedini@gmail.com>
2025-06-10 16:24:59 +0200

    Add edge cases for Backstage passes and improve test method names for clarity and consistency

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -11,7 +11,7 @@ class GildedRoseTest {
     private static final int SELL_IN_EXPIRES_TODAY = 0;
 
     @Test
-    void givenStandardItem_qualityAndSellInDecreaseEachDay() {
+    void givenStandardItem_whenUpdate_thenQualityAndSellInDecreaseEachDay() {
         int startingSellIn = 5;
         int startingQuality = 7;
         var standardItem = new Item("Elixir of the Mongoose", startingSellIn, startingQuality);
@@ -24,7 +24,7 @@ class GildedRoseTest {
     }
 
     @Test
-    void givenMultipleStandardItems_qualityAndSellInDecreaseEachDay() {
+    void givenMultipleStandardItems_whenUpdate_thenQualityAndSellInDecreaseEachDay() {
         var firstItem = new Item("First Standard Item", 5, 4);
         var secondItem = new Item("Second Standard Item", 3, 2);
         var gildedRose = new GildedRose(new Item[]{firstItem, secondItem});
@@ -38,7 +38,7 @@ class GildedRoseTest {
     }
 
     @Test
-    void givenStandardItem_whenSellInDateInPast_thenQualityDegradesTwiceAsFast() {
+    void givenStandardItem_whenSellInHasPast_thenQualityDegradesTwiceAsFast() {
         var sellInInPast = -1;
         Item item = new Item("Standard Item", sellInInPast, 4);
         var gildedRose = new GildedRose(new Item[]{item});
@@ -95,7 +95,7 @@ class GildedRoseTest {
 
 
     @Test
-    void givenAgedItem_whenUpdate_thenQualityIncrease() {
+    void givenAgedItem_whenUpdate_thenQualityIncreases() {
         var item = new Item("Aged Brie", 5, 6);
         var gildedRose = new GildedRose(new Item[]{item});
 
@@ -173,5 +173,115 @@ class GildedRoseTest {
         gildedRose.updateQuality();
 
         assertThat(item.sellIn).isEqualTo(5);
+    }
+
+    @Test
+    void givenBackstagePass_whenMoreThanTenDaysLeft_thenQualityIncreasesByOne() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(21);
+    }
+
+    @Test
+    void givenBackstagePass_whenExactlyElevenDaysLeft_thenQualityIncreasesByOne() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 11, 48);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(NEAR_MAX_QUALITY);
+    }
+
+    @Test
+    void givenBackstagePass_whenExactlyTenDaysLeft_thenQualityIncreasesByTwo() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(22);
+    }
+
+    @Test
+    void givenBackstagePass_whenQualityNearMaxAndSellInIsTenDaysOrLess_thenQualityCapsAtMax() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 10, NEAR_MAX_QUALITY);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+    }
+
+    @Test
+    void givenBackstagePass_when6DaysOrLessLeft_thenQualityIncreasesByTwo() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 6, 46);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(48);
+    }
+
+    @Test
+    void givenBackstagePass_whenOneDayLeft_thenQualityIncreasesByThree() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(23);
+    }
+
+    @Test
+    void givenBackstagePass_when5DaysOrLess_thenQualityIncreasesByThree() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 20);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(23);
+    }
+
+    @Test
+    void givenBackstagePass_whenFiveDaysLeftAndNearMaxQuality_thenCappedAtMaxQuality() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, 47);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+    }
+
+    @Test
+    void givenBackstagePass_whenFiveDaysLeftAndAtMaxOrAboveLimit_thenQualityRemainsAtMaxQuality() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 5, NEAR_MAX_QUALITY);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isEqualTo(MAX_QUALITY);
+    }
+
+    @Test
+    void givenBackstagePass_whenOnConcertDate_thenQualityDropsToZero() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", SELL_IN_EXPIRES_TODAY, 20);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isZero();
+    }
+
+    @Test
+    void givenBackstagePass_onConcertDateAndMaxQuality_qualityDropsToZero() {
+        var item = new Item("Backstage passes to a TAFKAL80ETC concert", 0, 50);
+        var gildedRose = new GildedRose(new Item[]{item});
+
+        gildedRose.updateQuality();
+
+        assertThat(item.quality).isZero();
     }
 }
