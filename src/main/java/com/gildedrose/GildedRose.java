a6bba4015bc3e8186b2d6111d572c78e06136614
shadi <shadiabedini@gmail.com>
2025-06-10 18:47:53 +0200

    Refactor: create ItemInventory base class to encapsulate shared update logics and prepare the code for polymorphic behavior

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -9,70 +9,7 @@ class GildedRose {
 
     public void updateQuality() {
         for (Item item : items) {
-            updateItem(item);
-        }
-    }
-
-    private void updateItem(Item item) {
-        updateQuality(item);
-        updateSellIn(item);
-        if (isExpired(item)) {
-            processItem(item);
-        }
-    }
-
-    private void updateSellIn(Item item) {
-        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        }
-        item.sellIn--;
-    }
-
-    private void updateQuality(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            increaseQuality(item);
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            increaseQuality(item);
-            if (item.sellIn < 11) {
-                increaseQuality(item);
-            }
-
-            if (item.sellIn < 6) {
-                increaseQuality(item);
-            }
-        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            return;
-        } else {
-            decreaseQuality(item);
-        }
-    }
-
-    private void processItem(Item item) {
-        if (item.name.equals("Aged Brie")) {
-            increaseQuality(item);
-        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-            item.quality = 0;
-        } else {
-            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                return;
-            }
-            decreaseQuality(item);
-        }
-    }
-
-    private boolean isExpired(Item item) {
-        return item.sellIn < 0;
-    }
-
-    private void increaseQuality(Item item) {
-        if (item.quality < 50) {
-            item.quality++;
-        }
-    }
-
-    private void decreaseQuality(Item item) {
-        if (item.quality > 0) {
-            item.quality = item.quality - 1;
+            new ItemInventory(item).updateItem(item);
         }
     }
 }
---
+++ src/main/java/com/gildedrose/ItemInventory.java
@@ -0,0 +1,72 @@
+package com.gildedrose;
+
+public class ItemInventory {
+    protected final Item item;
+
+    public ItemInventory(Item item) {
+        this.item = item;
+    }
+
+    protected void updateItem(Item item) {
+        updateQuality(item);
+        updateSellIn(item);
+        if (isExpired(item)) {
+            processItem(item);
+        }
+    }
+
+    protected void updateSellIn(Item item) {
+        if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        }
+        item.sellIn--;
+    }
+
+    protected void updateQuality(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            increaseQuality(item);
+
+            if (item.sellIn < 11) {
+                increaseQuality(item);
+            }
+
+            if (item.sellIn < 6) {
+                increaseQuality(item);
+            }
+
+        } else if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            return;
+        } else decreaseQuality(item);
+    }
+
+    protected void processItem(Item item) {
+        if (item.name.equals("Aged Brie")) {
+            increaseQuality(item);
+        } else if (item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+            item.quality = 0;
+        } else {
+            if (item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                return;
+            }
+            decreaseQuality(item);
+        }
+    }
+
+    protected boolean isExpired(Item item) {
+        return item.sellIn < 0;
+    }
+
+    protected void increaseQuality(Item item) {
+        if (item.quality < 50) {
+            item.quality++;
+        }
+    }
+
+    protected void decreaseQuality(Item item) {
+        if (item.quality > 0) {
+            item.quality = item.quality - 1;
+        }
+    }
+}
