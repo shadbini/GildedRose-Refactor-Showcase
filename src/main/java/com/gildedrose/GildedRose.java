13d13c5de261763eaa6849a2a1f62c4c3e9bc8cb
shadi <shadiabedini@gmail.com>
2025-06-10 17:15:33 +0200

    Refactor: split item update logic into separate methods for quality, sellIn, and post-expiry handling

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -14,6 +14,22 @@ class GildedRose {
     }
 
     private void updateItem(Item item) {
+        updateQuality(item);
+
+        updateSellIn(item);
+
+        if (item.sellIn < 0) {
+            processItem(item);
+        }
+    }
+
+    private static void updateSellIn(Item item) {
+        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+            item.sellIn = item.sellIn - 1;
+        }
+    }
+
+    private void updateQuality(Item item) {
         if (!item.name.equals("Aged Brie")
                 && !item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
             if (item.quality > 0) {
@@ -40,26 +56,22 @@ class GildedRose {
                 }
             }
         }
+    }
 
-        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-            item.sellIn = item.sellIn - 1;
-        }
-
-        if (item.sellIn < 0) {
-            if (!item.name.equals("Aged Brie")) {
-                if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
-                    if (item.quality > 0) {
-                        if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
-                            item.quality = item.quality - 1;
-                        }
+    private void processItem(Item item) {
+        if (!item.name.equals("Aged Brie")) {
+            if (!item.name.equals("Backstage passes to a TAFKAL80ETC concert")) {
+                if (item.quality > 0) {
+                    if (!item.name.equals("Sulfuras, Hand of Ragnaros")) {
+                        item.quality = item.quality - 1;
                     }
-                } else {
-                    item.quality = 0;
                 }
             } else {
-                if (item.quality < 50) {
-                    item.quality = item.quality + 1;
-                }
+                item.quality = 0;
+            }
+        } else {
+            if (item.quality < 50) {
+                item.quality = item.quality + 1;
             }
         }
     }
